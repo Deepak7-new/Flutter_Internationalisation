f9697aed56240aa7594d0def1b16dd9207b359da
Deepak7-new <55599102+Deepak7-new@users.noreply.github.com>
2020-03-31 15:43:20 +0530

    Update converted_text.dart

--- lib/widgets/converted_text.dart
+++ lib/widgets/converted_text.dart
@@ -1,3 +1,4 @@
+//I have not given translation in french for some keys to test google translator
 var dataMap = {
   "Hello world": {
     "en": "Hello world",
@@ -7,19 +8,22 @@ var dataMap = {
   "goodbye": {
     "en": "goodbye",
     "hi": "अलविदा",
-
+    "fr": "Au revoir"
   },
   "You typed it %d times": {
     "en": "You typed it %d times",
-    "hi": "आपने इसे %d बार टाइप किया"
+    "hi": "आपने इसे %d बार टाइप किया",
+    "fr": "Vous l'avez tapé% d fois"
   },
   "You typed it 0 times": {
     "en": "You haven't typed it",
-    "hi": "आपने इसे टाइप नहीं किया है"
+    "hi": "आपने इसे टाइप नहीं किया है",
+    "fr": "Vous ne l'avez pas tapé"
   },
   "You typed it 1 times": {
     "en": "You have typed it once",
-    "hi": "आपने इसे एक बार टाइप किया है"
+    "hi": "आपने इसे एक बार टाइप किया है",
+    "fr": "Vous l'avez tapé 1 fois"
   },
   "there is a person": {
     "en": "there is a person",
